bf722959a86da131033796314170e45fd18f9f20
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-07 13:28:41 +0530

    setup next-themes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,39 @@
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+        sans: ["Manrope", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#64DC0D",
+          50: "#CAFAA7",
+          100: "#BEF994",
+          200: "#A7F66D",
+          300: "#8FF447",
+          400: "#78F220",
+          500: "#64DC0D",
+          600: "#4CA70A",
+          700: "#347207",
+          800: "#1C3D04",
+          900: "#040800",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      fontFamily: ["hover", "focus"],
+    },
   },
   plugins: [],
+  corePlugins: {
+    fontFamily: true,
+  },
 };
