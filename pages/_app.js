fc2191f67c692de0926d517156f87218ba4a217c
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-12 10:51:50 +0530

    set meta: theme-color

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import NavigationBar from "../components/NavigationBar";
+import Footer from "../components/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+      <Head>
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <NavigationBar />
       <Component {...pageProps} />
       <Footer />
     </ThemeProvider>
