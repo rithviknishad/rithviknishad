dfb6991942de6c1dae3dbee394d2c90cb2c34f31
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-12 10:51:03 +0530

    add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { GitHub } from "./SocialButtons";
+
+export default function Footer(props) {
+  return (
+    <section className="w-full p-10 bg-black">
+      <GitHub username="rithviknishad" />
+    </section>
+  );
+}
