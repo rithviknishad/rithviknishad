bb2955479ed6a8c77ce1e4c438c7cca9ab3f2cd6
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-12 12:31:47 +0530

    update header section

--- pages/index.js
+++ pages/index.js
@@ -25,34 +25,39 @@ const templateData = {
   ],
 };
 
-{
-  /* <PrimaryButton Icon={BsArrowRight}>ABOUT ME</PrimaryButton>
-      <div className="py-10"></div>
-      <SecondaryButton Icon={BsArrowRight}>SECONDARY BUTTON</SecondaryButton> */
-}
-
 export default function Home() {
   return (
     <div className="scroll-smooth">
       {/* Header */}
-      <section className="flex flex-col justify-center relative h-screen dark:bg-neutral-800 pt-8 px-8 lg:overflow-hidden bg-hero-pattern bg-repeat-x bg-cover scales bg-blend-multiply dark:bg-blend-color-burn">
+      <section className="flex flex-col justify-center relative min-h-screen dark:bg-neutral-800 pt-8 px-8 lg:overflow-hidden bg-hero-pattern bg-repeat-x bg-cover scales bg-blend-multiply dark:bg-blend-color-burn">
         <div className="container relative lg:min-h-screen flex flex-col lg:flex-row lg:items-center mx-auto max-w-screen-xl z-10 -mb-16">
           <div className="w-full lg:w-1/2 flex justify-center self-center">
-            <Image
-              className="rounded-full"
-              src="/images/profile-picture.jfif"
-              width={200}
-              height={200}
-              alt="Photo of Rithvik Nishad"
-            />
+            <div className="w-40 sm:w-56 md:w-60 lg:w-72 xl:w-80 aspect-square relative">
+              <Image
+                className="rounded-full"
+                src="/images/profile-picture.jfif"
+                layout="fill"
+                objectFit="cover"
+                alt="Photo of Rithvik Nishad"
+              />
+            </div>
           </div>
           <div className="w-full lg:w-1/2">
-            <h1 className="text-primary border-t border-gray-100 dark:border-gray-500 mt-6 pt-6 lg:border-t-0 lg:pt-0 leading-none">
+            <h1 className="text-primary mt-6 pt-6 lg:pt-0 leading-none">
               Rithvik Nishad
             </h1>
             <p className="text-gray-900 font-bold dark:text-gray-200 my-10 text-lg xl:text-xl leading-tight">
               Software Developer . Electrical Engineer . Creator
             </p>
+            <p className="text-gray-700 text-base md:text-md dark:text-gray-400 mb-4 leading-relaxed max-w-prose">
+              {`Passionate about creating scalable and distributed systems for 
+              the power ⚡ grid and contributing to open source digital public 
+              goods.`}
+            </p>
+            <div className="flex flex-wrap items-start gap-4 pb-24 lg:pb-0 mt-8">
+              <PrimaryButton Icon={BsArrowRight}>PROJECTS</PrimaryButton>
+              <SecondaryButton Icon={BsArrowRight}>KNOW ME</SecondaryButton>
+            </div>
           </div>
         </div>
       </section>
