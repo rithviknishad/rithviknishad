41e98afdafc5b2407f9eaf19d5139ceec1b39a53
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-12 14:39:22 +0530

    update title

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
-import Script from "next/script";
 import { BsArrowRight } from "react-icons/bs";
 import { PrimaryButton, SecondaryButton } from "../components/Buttons";
-import Footer from "../components/Footer";
-import NavigationBar from "../components/NavigationBar";
-
-const templateData = {
-  name: "Rithvik Nishad",
-  about: "Rebuilding the power grid ⚡",
-  primaryColor: "black",
-  socialLinks: [
-    "https://github.com/rithviknishad",
-    "https://www.linkedin.com/in/rithviknishad/",
-    "https://twitter.com/rithviknishad",
-  ],
-  repositories: [
-    "Vaidyuti/vems",
-    "rithviknishad/prosumer",
-    "coronasafe/care_fe",
-    "osumpi/osumpi",
-    "osumpi/bakecode",
-    "osumpi/recipe",
-  ],
-};
 
 export default function Home() {
   return (
     <div className="scroll-smooth">
+      <Head>
+        <title>
+          {"Rithvik Nishad – software developer, electrical engineer, creator"}
+        </title>
+      </Head>
       {/* Header */}
       <section className="flex flex-col justify-center relative min-h-screen dark:bg-neutral-800 pt-8 px-8 lg:overflow-hidden bg-hero-pattern bg-repeat-x bg-cover scales bg-blend-multiply dark:bg-blend-color-burn">
         <div className="container relative lg:min-h-screen flex flex-col lg:flex-row lg:items-center mx-auto max-w-screen-xl z-10 -mb-16">
