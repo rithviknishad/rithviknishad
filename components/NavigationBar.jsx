1cc76d462e84c8cc2419918c1ba3b7793d9552ff
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-13 12:52:24 +0530

    partial nav bar implementation

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -1,6 +1,7 @@
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function NavigationBar() {
+export default function NavigationBar({ locations }) {
   const [shrinked, setShrinked] = useState(false);
 
   useEffect(() => {
@@ -14,34 +15,33 @@ export default function NavigationBar() {
     return () => window.removeEventListener("scroll", onScroll);
   });
 
-  useEffect(() => {
-    console.log(shrinked);
-  }, [shrinked]);
-
   return (
     <nav
       className={
         shrinked
-          ? "navbar bg-black border-b border-neutral-700 py-4"
-          : "navbar border-black border-b-0 py-8"
+          ? "navbar nav-primary bg-black border-b border-neutral-700 py-4"
+          : "navbar nav-primary border-black border-b-0 py-8"
       }
     >
       <div></div>
-      <span>{String(shrinked)}</span>
-    </nav>
-  );
-
-  return (
-    <div
-      className={
-        shrinked
-          ? "bg-black border-b border-zinc-400"
-          : "bg-white border-b-4 border-red-400"
-      }
-    >
-      <div className="flex items-center justify-between w-full p-8 mx-auto my-0 fixed z-30 transition-all">
-        {String(shrinked)}
+      <div className="flex items-center transition-all">
+        <div className="hidden md:block">
+          {locations.map((location) => (
+            <a
+              key={location.label}
+              className="p-1 text-base font-bold text-gray-900 dark:text-gray-100 sm:p-4 tracking-wider cursor-pointer"
+              onClick={() =>
+                location.ref.current.scrollIntoView({
+                  behaviour: "smooth",
+                  block: "start",
+                })
+              }
+            >
+              {location.label}
+            </a>
+          ))}
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
