d102f7c7ecb1acc4880164b175ad4d9043dba059
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-13 12:52:13 +0530

    update footer tagline

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -18,7 +18,7 @@ export default function Footer(props) {
         <section>
           <hr className="lg:hidden mb-6 border-gray-200 dark:border-gray-800" />
           <h2 className="md:text-center lg:text-right text-gray-600 dark:text-gray-400 text-sm md:text-base">
-            Fork, contribute, follow, like, clone
+            Fork, connect, like, follow - me
           </h2>
           <div className="flex flex-wrap md:justify-center items-center gap-8 mt-6">
             <GitHub username="rithviknishad" />
