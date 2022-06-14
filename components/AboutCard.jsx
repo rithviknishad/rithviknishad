ed4514ed34de03575f02fe95a876e6daa7139752
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-14 22:15:08 +0530

    improvements here and there

--- components/AboutCard.jsx
+++ components/AboutCard.jsx
@@ -10,13 +10,17 @@ export default function AboutCard({ about }) {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <li className="flex items-start hover:dark:text-white transition-all ease-out text-gray-800 dark:text-gray-400 text-base">
+      <li className="group flex items-start hover:dark:text-white lg:p-4 rounded-lg transition-all ease-out text-gray-800 dark:text-gray-400 text-base hover:bg-opacity-75 hover:bg-primary-900">
         <div className="min-w-fit text-2xl mt-5">{icon}</div>
         <div className="flex flex-col pl-3 gap-1">
-          <span className="uppercase text-xs tracking-wider">{timeline}</span>
+          <span className="uppercase text-xs tracking-wider group-hover:font-bold group-hover:text-sm transition-all ease-in-out">
+            {timeline}
+          </span>
           <div className="flex justify-between items-center">
             <span className="font-bold tracking-wide lg:text-xl">{title}</span>
-            {link && <GoLinkExternal className="text-primary" />}
+            {link && (
+              <GoLinkExternal className="text-primary group-hover:animate-pulse lg:text-xl" />
+            )}
           </div>
           <span className="uppercase text-xs lg:text-sm tracking-wider">
             {subtitle}
