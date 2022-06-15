cc426dc449a5f6af4b0983bdfef2de713073bf3f
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-15 12:19:37 +0530

    adds marquee animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        marquee: "marquee 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
       fontFamily: {
         manrope: ["Manrope", "sans-serif"],
         sans: ["Manrope", "sans-serif"],
