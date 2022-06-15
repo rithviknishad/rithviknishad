a14abd503fe674e80eda938ef0dfaff43bbc5818
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-15 14:44:19 +0530

    skills section improvements

--- components/SkillCard.jsx
+++ components/SkillCard.jsx
@@ -3,20 +3,23 @@ export default function SkillCard({ category, skills }) {
 
   for (const skillKey in skills) {
     renderedSkills.push([
-      <div
+      <a
         key={skillKey}
-        className="flex flex-row items-center gap-2 hover:dark:text-primary w-40"
+        href={skills[skillKey].link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex flex-row items-center gap-2 hover:dark:text-zinc-900 hover:bg-white w-40 px-2 py-1 transition-all ease-in-out rounded"
       >
         {skills[skillKey].icon}
         <span className="text-xs md:text-base tracking-wide">
           {skills[skillKey].name}
         </span>
-      </div>,
+      </a>,
     ]);
   }
 
   return (
-    <div className="group flex items-start hover:dark:text-white lg:p-4 rounded-lg transition-all ease-out text-gray-800 dark:text-gray-400 text-base hover:bg-opacity-75 hover:bg-primary-900">
+    <div className="group flex items-start lg:hover:dark:text-white lg:p-4 rounded-lg transition-all ease-out text-gray-800 dark:text-gray-400 text-base lg:hover:bg-opacity-75 lg:hover:bg-primary-900">
       {/* <div className="min-w-fit text-2xl mt-5">{icon}</div> */}
       <div className="flex flex-col gap-1">
         <div className="flex justify-between items-center">
@@ -27,7 +30,7 @@ export default function SkillCard({ category, skills }) {
         <span className="uppercase text-xs lg:text-sm tracking-wider">
           {/* {subtitle} */}
         </span>
-        <div className="mt-3 grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="mt-3 grid grid-cols-2 md:grid-cols-3 gap-3">
           {renderedSkills}
         </div>
       </div>
