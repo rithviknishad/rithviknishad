1eb9a1711ca0e3ee01b6d12498f988345eac4c0f
Rithvik Nishad <rithvikn2001@gmail.com>
2022-06-15 16:35:12 +0530

    supports now blink in about

--- components/AboutCard.jsx
+++ components/AboutCard.jsx
@@ -1,7 +1,7 @@
 import { GoLinkExternal } from "react-icons/go";
 
 export default function AboutCard({ about }) {
-  const { timeline, icon, title, subtitle, description, link } = about;
+  const { timeline, icon, title, subtitle, description, link, now } = about;
 
   return (
     <a
@@ -13,8 +13,16 @@ export default function AboutCard({ about }) {
       <li className="group flex items-start hover:dark:text-white lg:p-4 rounded-lg transition-all ease-out text-gray-800 dark:text-gray-400 text-base lg:hover:bg-opacity-75 lg:hover:bg-primary-900">
         <div className="min-w-fit text-2xl mt-5">{icon}</div>
         <div className="flex flex-col pl-3 gap-1">
-          <span className="uppercase text-xs tracking-wider group-hover:font-bold lg:group-hover:text-sm transition-all ease-in-out">
-            {timeline}
+          <span>
+            <span className="relative inline-flex uppercase text-xs tracking-wider group-hover:font-bold lg:group-hover:text-sm transition-all ease-in-out">
+              {timeline}
+              {now && (
+                <span className="flex absolute h-3 w-3 top-1/2 right-0 -mt-1.5 -mr-5 items-center justify-center">
+                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-primary-400 opacity-75"></span>
+                  <span className="relative inline-flex rounded-full h-2 w-2 bg-primary-500"></span>
+                </span>
+              )}
+            </span>
           </span>
           <div className="flex items-center">
             <span className="font-bold tracking-wide lg:text-xl pr-3">
