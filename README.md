dae8780fbb5aefa05d31d778475917f3d8c0cbc6
rithviknishad <mail@rithviknishad.dev>
2026-06-29 10:57:55 +0530

    remove everything

--- README.md
+++
@@ -1,4 +0,0 @@
-<div>
-<img src="https://raw.githubusercontent.com/rithviknishad/github-stats/master/generated/overview.svg" />
-<img src="https://raw.githubusercontent.com/rithviknishad/github-stats/master/generated/languages.svg" />
-</div>
--- src/components/Time.tsx
+++
@@ -1,9 +0,0 @@
-import { relativeTime } from "@/utils/dayjs";
-
-export default function Time({ time }: { time: string }) {
-  return (
-    <time dateTime={time} title={new Date(time).toString()}>
-      {relativeTime(time)}
-    </time>
-  );
-}
